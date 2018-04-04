e4bb0552a0e5c606ad20eafaa027f281fca7fae8
favoritas37 <ftylitak@gmail.com>
2018-04-04 13:40:15 +0300

    Updated package.json with the needed npm nodules that will be initially used. That is:  - chai & mocha: for unit testing  - eslint (plus plugins): for code analysis (linter)  - lodash: useful utilities
    Added the generated package-lock.json
    
    Added .eslintrc.js with the eslint rules that will be used in this project.

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+    "env": {
+        "browser": false,
+        "es6": true,
+        "node": true,
+        "mocha": true
+    },
+    "plugins": [
+      "node",
+      "mocha"
+    ],
+    "ecmaFeatures": { "modules": true },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module",
+        "jsx": true
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        'no-console': 'off',
+        "mocha/no-exclusive-tests": "error"
+    }
+}
+;
